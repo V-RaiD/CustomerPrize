4b59f463d5628ac2f5ab71f7dec88a31ace24760
Amitesh Rai <amitesh.rai@geminisolutions.in>
2018-07-06 00:33:03 +0530

    Initialised repo

---
+++ src/main/java/com/amitesh/prize/App.java
@@ -0,0 +1,13 @@
+package com.amitesh.prize;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
